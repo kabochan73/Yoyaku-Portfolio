3d26e99acbbc2609dc3f0fbb2428bceee213ae75
kabochan73 <kabochan7355@gmail.com>
2026-07-07 21:50:28 +0900

    Revert "Add Sentry error monitoring to the Laravel backend"
    This reverts commit 60b9e722a9fd2daef972b1f948819132eaf3c401.

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -29,6 +28,4 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-
-        Integration::handles($exceptions);
     })->create();
